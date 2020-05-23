5942022c0ae02a98457743114b87dc77d17b0646
limiRatnayake <42302128+limiRatnayake@users.noreply.github.com>
2020-05-24 00:24:16 +0530

    changes

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTvCus3v71DPEsKzQzVooJbV5e9L_mJqs",
+    authDomain: "final-year-project-a89ff.firebaseapp.com",
+    databaseURL: "https://final-year-project-a89ff.firebaseio.com",
+    projectId: "final-year-project-a89ff",
+    storageBucket: "final-year-project-a89ff.appspot.com",
+    messagingSenderId: "707195683377",
+    appId: "1:707195683377:web:157a808faf5a4c78e8cb10"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,43 @@
-// let formSignIn = $("#formSignIn");
-
-// let txtPassword = $("#txtPassword");
-// let txtEmail = $("#txtEmail");
-
-$("#formSignIn").validate({
-    rules: {
-        txtEmail: {
-            required: true,
-            email: true
-        },
-        txtPassword: {
-            required: true,
-            minlength: 6
+$(function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            Cookies.set("__session", user._lat);
+            window.location.href = window.location.origin + "/main/events";
         }
-    }
-});
+    });
+
+    $("#formSignIn").validate({
+        rules: {
+            txtEmail: {
+                required: true,
+                email: true
+            },
+            txtPassword: {
+                required: true,
+                minlength: 6,
+            }
+        }
+    });
 
 
-$(document).on("click", "#btnLogin", () => {
-    if ($("#formSignIn").valid()) {
-        alert("Valid: ");
-        $("#txtEmail").val(),
-            $("#txtPassword").val()
-            // window.location.href = window.location.origin + "/main/events";
-    }
+
+    $(document).on("click", "#btnLogin", () => {
+        if ($("#formSignIn").valid()) {
+            firebase.auth().signInWithEmailAndPassword($("#txtEmail").val(), $("#txtPassword").val())
+                .then(result => {
+                    // Cookies.set("session", result.user._lat);
+                    window.location.href = window.location.origin + "/main/events";
+                }).catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+
+                    window.alert("Error:" + errorMessage);
+                });
+
+
+
+        }
+    });
+
 });
