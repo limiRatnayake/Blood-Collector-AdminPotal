d97d471fe3a07361308992c2c1cef6dc3fb15372
Limani Ratnayake <lpw.ratnayake@gmail.com>
2020-11-06 23:06:37 +0530

    cv

--- functions/index.js
+++ functions/index.js
@@ -34,7 +34,7 @@ exports.app = functions.https.onRequest(app);
 //push notification
 var newData;
 var tokens;
-let userId;
+// var userId;
 
 exports.messageTrigger = functions.firestore
    .document("notifications/{notificationId}")
@@ -45,70 +45,156 @@ exports.messageTrigger = functions.firestore
          .collection("users")
          .where("bloodGroup", "==", notificationData.bloodGroup)
          .get();
-
+      let userList = [];
       userRef.forEach(async (userSnapshot) => {
-         userId = userSnapshot.id;
+         // userId = userSnapshot.id;
+         userList.push(userSnapshot.id);
       });
-      console.log(userId);
-      const tokensQuerySnapshot = await db
-         .collection("users")
-         .doc(userId)
-         .collection("deviceTokens")
-         .get();
-
-      for (var tokenData of tokensQuerySnapshot.docs) {
-         tokens = [];
-         tokens.push(tokenData.data().token);
-      }
-
-      var payLoad = {
-         notification: {
-            title: "Request to Donate Blood",
-            body: "Click to see more",
-            sound: "default",
-         },
-
-         data: {
-            click_action: "FLUTTER_NOTIFICATION_CLICK",
-            message: notificationData.message,
-            title: "Your title",
-            body: "Your body",
-         },
-      };
-      //send data to device
-      try {
-         const response = firebaseAdmin
-            .messaging()
-            .sendToDevice(tokens, payLoad);
-
-         db.collection("users")
+      console.log(userList);
+      for (var userId of userList) {
+         const tokensQuerySnapshot = await db
+            .collection("users")
             .doc(userId)
-            .collection("user_notification")
-            .doc();
+            .collection("deviceTokens")
+            .get();
+         //(for/of )loops through the values of an iterable object
+         for (var tokenData of tokensQuerySnapshot.docs) {
+            tokens = [];
+            tokens.push(tokenData.data().token);
+         }
 
-         userNotifyRef.set({
-            notifyId: userNotifyRef.id,
-            docRef: notificationData.docRef,
-            notifyBy: notificationData.uid,
-            notificationId: notificationData.notificationId,
-            message: notificationData.message,
-            hospitalName: notificationData.hospitalName,
-         });
+         var payLoad = {
+            notification: {
+               title: "Request to Donate Blood",
+               body: "Click to see more",
+               sound: "default",
+            },
 
-         var userNotificationRef = db.collection("users").doc(userId);
-         var eventRef = db.collection("events").doc(notificationData.docRef);
+            data: {
+               click_action: "FLUTTER_NOTIFICATION_CLICK",
+               message: notificationData.message,
+               title: "Your title",
+               body: "Your body",
+            },
+         };
+         //send data to device
+         try {
+            const response = firebaseAdmin
+               .messaging()
+               .sendToDevice(tokens, payLoad);
 
-         //add a count when a user get new notification
-         userNotificationRef.update({
-            notificationCount: FieldValue.increment(1),
-         });
-         //get the total notification sent
-         eventRef.update({
-            notifyCount: userRef.size,
-         });
+            var userNotifyRef = db
+               .collection("users")
+               .doc(userId)
+               .collection("user_notification")
+               .doc();
 
-         console.log("Notification send successfully");
-      } catch (error) {
-         console.log(error);
+            userNotifyRef.set({
+               notifyId: userNotifyRef.id,
+               docRef: notificationData.docRef,
+               notifyBy: notificationData.uid,
+               notificationId: notificationData.notificationId,
+               message: notificationData.message,
+               hospitalName: notificationData.hospitalName,
+            });
+
+            var userNotificationRef = db.collection("users").doc(userId);
+            var eventRef = db.collection("events").doc(notificationData.docRef);
+
+            //add a count when a user get new notification
+            userNotificationRef.update({
+               notificationCount: FieldValue.increment(1),
+            });
+            //get the total notification sent
+            eventRef.update({
+               notifyCount: userRef.size,
+            });
+
+            console.log("Notification send successfully");
+         } catch (error) {
+            console.log(error);
+         }
       }
+      // for (var tokenData of tokensQuerySnapshot.docs) {
+      //    tokens = [];
+      //    tokens.push(tokenData.data().token);
+      // }
    });
+// exports.messageTrigger = functions.firestore
+// .document("notifications/{notificationId}")
+// .onCreate(async (snapshot, context) => {
+//    const notificationData = snapshot.data();
+
+//    const userRef = await db
+//       .collection("users")
+//       .where("bloodGroup", "==", notificationData.bloodGroup)
+//       .get();
+
+//    userRef.forEach(async (userSnapshot) => {
+//       userId = userSnapshot.id;
+//    });
+
+//    const tokensQuerySnapshot = await db
+//       .collection("users")
+//       .doc(userId)
+//       .collection("deviceTokens")
+//       .get();
+
+//    for (var tokenData of tokensQuerySnapshot.docs) {
+//       tokens = [];
+//       tokens.push(tokenData.data().token);
+//    }
+
+//    var payLoad = {
+//       notification: {
+//          title: "Request to Donate Blood",
+//          body: "Click to see more",
+//          sound: "default",
+//       },
+
+//       data: {
+//          click_action: "FLUTTER_NOTIFICATION_CLICK",
+//          message: notificationData.message,
+//          title: "Your title",
+//          body: "Your body",
+//       },
+//    };
+//    //send data to device
+//    try {
+//       const response = firebaseAdmin
+//          .messaging()
+//          .sendToDevice(tokens, payLoad);
+
+//       var userNotifyRef = db
+//          .collection("users")
+
+//          .doc(userId)
+//          .collection("user_notification")
+//          .doc();
+
+//       userNotifyRef.set({
+//          notifyId: userNotifyRef.id,
+//          docRef: notificationData.docRef,
+//          notifyBy: notificationData.uid,
+//          notificationId: notificationData.notificationId,
+//          message: notificationData.message,
+//          hospitalName: notificationData.hospitalName,
+//       });
+
+//       var userNotificationRef = db.collection("users").doc(userId);
+//       var eventRef = db.collection("events").doc(notificationData.docRef);
+
+//       //add a count when a user get new notification
+//       userNotificationRef.update({
+//          notificationCount: FieldValue.increment(1),
+//       });
+//       //get the total notification sent
+//       eventRef.update({
+//          notifyCount: userRef.size,
+//       });
+
+//       console.log("Notification send successfully");
+//    } catch (error) {
+//       console.log(error);
+//    }
+// });
